fd36076e2bbe0b4d6eac5e706a4ea71854288772
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-02-04 22:15:26 +0700

    Create fibonacci.c

---
+++ fibonacci.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int i,num,a=0.b=1,c;
+    printf("Enter number:");
+    scanf("%d",&num);
+    printf("Fibonacci Series:");
+    for(i=0;i<=num;i++){
+        printf("%d ",a);
+        c=a+b;
+        a=b;
+        b=c;
+    }
+}
