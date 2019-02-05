5d88b680284f00ee480ca014cf76f33ce133a0b8
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-02-05 18:16:56 +0700

    created

---
+++ pattern4.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int i,j,n;
+	printf("Enter the height of the triangular pattern:");
+	scanf("%d",&n);
+	for(i=1;i<=n;i++){
+		for(j=1;j<=n-i;j++)
+			printf(" ");
+		for(j=1;j<=i;j++)
+			printf("*");
+		printf("\n");
+	}
+}
