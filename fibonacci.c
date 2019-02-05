b492ef7c2e67f753e64751f497100543da9dc2df
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-02-05 17:58:50 +0700

    update

--- fibonacci.c
+++ fibonacci.c
@@ -1,14 +1,15 @@
-#include<stdio.h>
+#include <stdio.h>
 void main(){
-    int f1=0,f2=1,f3,i=3,num;
-    printf("Enter length of the  fibonacci series:");
-    scanf("%d",&num);
-    printf("%d %d",f1,f2); 
-    while(i<=num){
-        f3=f1+f2;
-        printf(" %d",f3);
-        f1=f2;
-        f2=f3;
-        i++;
+    int t1=0,t2=1,t3=0,num;
+    printf("Enter a positive number: ");
+    scanf("%d", &num);
+    printf("Fibonacci Series: %d, %d, ", t1, t2);
+    t3 = t1 + t2;
+    while(t3 <= num){
+        printf("%d, ",t3);
+        t1 = t2;
+        t2 = t3;
+        t3 = t1 + t2;
     }
+    printf("\n");
 }
