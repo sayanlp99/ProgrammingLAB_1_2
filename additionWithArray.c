fe1f399c8c78171e0b672f24e9dc1de94c54409c
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-02-05 18:31:19 +0700

    created

---
+++ additionWithArray.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int a[50],i,n,sum=0;
+    printf("Enter the number of elements to be added:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("Enter Element:");
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<=n;i++)
+        sum=sum+a[i];
+    printf("Sum of all the elemets is:%d\n",sum);
+}
