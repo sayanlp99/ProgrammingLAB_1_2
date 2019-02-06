a4bcdc2af300a5b5c28f9278d60326f241a78416
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-02-06 20:57:36 +0700

    update

---
+++ Python/factorial.py
@@ -0,0 +1,8 @@
+def factorial(number):
+    if number == 1 or number == 0:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
+number = int(input())
+print(factorial(number))
