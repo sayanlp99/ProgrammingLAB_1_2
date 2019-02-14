0018b57cdd205ccbd832b7f7d183bbfd3b584aae
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-02-14 23:22:38 +0700

    update

--- C/additionAndAverageWithArray.c
+++ C/additionAndAverageWithArray.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void main(){
-    int a[50],i,n,sum=0;
+    int a[50],i,n,sum=0,avg;
     printf("Enter the number of elements to be added:");
     scanf("%d",&n);
     for(i=0;i<n;i++){
@@ -9,5 +9,7 @@ void main(){
     }
     for(i=0;i<=n;i++)
         sum=sum+a[i];
+    avg=sum/n;
     printf("Sum of all the elements is:%d\n",sum);
+    printf("Average of all the elements:%d\n",avg);
 }
