35dbd33dabffc8810672b04b5b815763e38e50f5
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-03-13 21:50:45 +0800

    update

--- C/series.c
+++ C/series.c
@@ -1,18 +1,17 @@
 #include<stdio.h>
 #include<math.h>
-int fact(float);
+float fact(float);
 void main(){
-    float n,i;
-    float s=0;
+    float n,i,s=0;
     printf("Enter the value of n:");
     scanf("%f",&n);
     for(i=1;i<=n;i++){
         s=(pow(i,i)/fact(i))+s;
     }
-    printf("Value of the Series upto %f:%f\n",n,s);
+    printf("Value of the Series:%f\n",s);
 }
-int fact(float a){
-    float f;
+float fact(float a){
+    float f=1;
     for(float x=1;x<=a;x++){
         f=f*x;
     }
