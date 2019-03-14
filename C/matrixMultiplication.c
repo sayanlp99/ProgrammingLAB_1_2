c8cc63b66691a8eef3f9c883cb036c708355274e
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-03-15 02:41:14 +0800

    update

--- C/matrixMultiplication.c
+++ C/matrixMultiplication.c
@@ -5,39 +5,43 @@ void main(){
     scanf("%d %d", &r1, &c1);
     printf("Enter rows and column for second matrix: ");
     scanf("%d %d",&r2, &c2);
-    printf("\nEnter elements of matrix 1:\n");
-    for(i=0; i<r1; ++i){
-        for(j=0; j<c1; ++j){
-            printf("Enter elements a%d%d: ",i+1, j+1);
-            scanf("%d", &a[i][j]);
+    if(r1==c2){
+        printf("\nEnter elements of matrix 1:\n");
+        for(i=0; i<r1; ++i){
+            for(j=0; j<c1; ++j){
+                printf("[%d][%d]: ",i+1, j+1);
+                scanf("%d", &a[i][j]);
+            }
         }
-    }
-    printf("\nEnter elements of matrix 2:\n");
-    for(i=0; i<r2; ++i){
-        for(j=0; j<c2; ++j){
-            printf("Enter elements b%d%d: ",i+1, j+1);
-            scanf("%d",&b[i][j]);
+        printf("\nEnter elements of matrix 2:\n");
+        for(i=0; i<r2; ++i){
+            for(j=0; j<c2; ++j){
+                printf("[%d][%d]: ",i+1, j+1);
+                scanf("%d",&b[i][j]);
+            }
         }
-    }
-    for(i=0; i<r1; ++i){
-        for(j=0; j<c2; ++j)
-        {
-            result[i][j] = 0;
+        for(i=0; i<r1; ++i){
+            for(j=0; j<c2; ++j)
+            {
+                result[i][j] = 0;
+            }
         }
-    }
-    for(i=0; i<r1; ++i){
-        for(j=0; j<c2; ++j){
-            for(k=0; k<c1; ++k){
-                result[i][j]+=a[i][k]*b[k][j];
+        for(i=0; i<r1; ++i){
+            for(j=0; j<c2; ++j){
+                for(k=0; k<c1; ++k){
+                    result[i][j]+=a[i][k]*b[k][j];
+                }
+            }
+        }
+        printf("\nOutput Matrix:\n");
+        for(i=0; i<r1; ++i){
+            for(j=0; j<c2; ++j){
+                printf("%d  ", result[i][j]);
+                if(j == c2-1)
+                    printf("\n\n");
             }
         }
     }
-    printf("\nOutput Matrix:\n");
-    for(i=0; i<r1; ++i){
-        for(j=0; j<c2; ++j){
-            printf("%d  ", result[i][j]);
-            if(j == c2-1)
-                printf("\n\n");
-        }
-    }
+    else
+        printf("Column of Matrix1 doesn't match to Matrix2.\n");
 }
