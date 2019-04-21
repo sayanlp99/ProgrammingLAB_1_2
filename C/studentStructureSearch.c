86ba29f9224858a6e469a51954b8bd043a713f53
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-04-21 22:17:18 +0530

    update

--- C/studentStructureSearch.c
+++ C/studentStructureSearch.c
@@ -8,6 +8,7 @@ struct student{
 }s[100];
 void main(){
     int i,n;
+    char t[50];
     printf("Enter number of students:");
     scanf("%d",&n);
     printf("Enter information of students:-\n\n");
@@ -21,4 +22,15 @@ void main(){
         printf("Enter marks:");
         scanf("%d",&s[i].marks);
         printf("\n");
+        fflush(stdin);
     }
+    printf("Enter name of student to be searched:");
+    gets(t);
+    for(i=0;i<n;i++){
+        if(strcmp(s[i].name,t)==0){
+            printf("Name:");
+            puts(s[i].name);
+            printf("Roll No:%d\nMarks:%d\n",s[i].roll,s[i].marks);
+        }
+    }
+}
