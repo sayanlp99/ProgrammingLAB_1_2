191829dcbfe36d7ae066162b38a347b4ffcf8ebb
Sayan Chakraborty <sayan.chakraborty1999@gmail.com>
2019-04-23 20:32:53 +0530

    update

---
+++ C/callocRealloc.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+void main(){
+    int n,*p,i;
+    printf("Enter range:");
+    scanf("%d",&n);
+    p=(int*)calloc(n,sizeof(int));
+    for(i=0;i<n;i++)
+        scanf("%d",p+i);
+    printf("Elements:\n");
+    for(i=0;i<n;i++)
+        printf("%d ",*(p+i));
+    printf("\n");
+    printf("Enter Range:");
+    scanf("%d",&n);
+    p=(int*)realloc(p,n*sizeof(int));
+    for(;i<n;i++)
+        scanf("%d",p+i);
+    printf("Elements:\n");
+    for(i=0;i<n;i++)
+        printf("%d ",*(p+i));
+}
